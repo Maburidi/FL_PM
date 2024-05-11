722dc5eec109e607289df5c0b396f9a2ee00dddb
Mohammed Aburidi <48891624+Maburidi@users.noreply.github.com>
2024-05-11 15:15:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ from Datasets import Dataset, PrePtbDataset
 from defense import * 
 from defense import GCN 
 from utils import * 
-from models import ProjMetattack 
+from models import FL_PMttack  
 
 def Parser():
     parser = argparse.ArgumentParser(description="RobustFM")
@@ -54,16 +54,13 @@ def Parser():
     return parser
 
 def main(args):
-    #no_cuda = args.no_cuda
     ptb_rate = args.ptb_rate
     seed = args.seed
     dataset = args.dataset
-    #attack = args.attack   #random, meta, nettack
     hidden = args.hidden
     dropout = args.dropout
     epochs =  args.epochs
     
-    #only_gcn= only_gcn.args
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     np.random.seed(seed)
@@ -71,51 +68,65 @@ def main(args):
     torch.cuda.manual_seed(seed)
 
     ############### DATA - import data ##############
-    data = Dataset(root='/content', name='cora', seed=15)
+ 
+    data = Dataset(root='/content', name='citeseer', seed=15)
     adj, features, labels = data.adj, data.features, data.labels
+    features = normalize_feature(features)
+
     idx_train, idx_val, idx_test = data.idx_train, data.idx_val, data.idx_test
-    idx_unlabeled = np.union1d(idx_val, idx_test)
+    #idx_train, idx_val, idx_test = get_train_val_test(adj.shape[0], val_size=0.1, test_size=0.8)
+    #idx_train, idx_val, idx_test = get_train_val_test(adj.shape[0], val_size=0.54126, test_size=0.40241)
+
+    ##### Set the targeted model #####
+    target_gcn = GCN(nfeat=features.shape[1],nhid=16, nclass=labels.max().item() + 1, dropout=0.5, device=device, lr=0.01)
+    target_gcn = target_gcn.to(device)
+
+    #--- train the model on clean data - and test it, print accuracy
+    target_gcn.fit(features, adj, labels, idx_train, idx_val,train_iters=200 , patience=300)
+    print('=== testing GCN on clean graph ===')
+    print(f" Accuracy = {test(adj, target_gcn)}")
+
+    #---- get predictions of all nodes
+    fake_labels = target_gcn.predict(features.to(device), adj.to(device))
+    fake_labels = torch.argmax(fake_labels, 1).cpu()
+    # Besides, we need to add the idx into the whole process
+    idx_fake = np.concatenate([idx_train,idx_test])
+    
 
     ###### set the perturbation rate #####
     perturbations = int(ptb_rate * (adj.sum()//2))                # number of edges to be perturbated
-    adj, features, labels = preprocess(adj, features, labels, preprocess_adj=False)
 
-    ###### Set the targeted model ##########
-    surrogate = GCN(nfeat=features.shape[1],nhid=16, nclass=labels.max().item() + 1, dropout=dropout, device=device,
-                 with_relu=False, with_bias=True, weight_decay=5e-4, lr=0.01)
-    # train it on clean data
-    surrogate = surrogate.to(device)
-    surrogate.fit(features, adj, labels, idx_train)
-
-
-    ####### Perform the attack on the graph data
-    lambda_ = 1
-    model = ProjMetattack(model=surrogate, nnodes=adj.shape[0], feature_shape=features.shape,  attack_structure=True, attack_features=False, device=device, lambda_=lambda_)
+    ####### SET UP ATTACK MODEL #######
+    print('=== setup attack model ===')
+    model = FL_PMttack(model=target_gcn, nnodes=adj.shape[0], loss_type='FL', device=device)
     model = model.to(device)
-    model.attack(features, adj, labels, idx_train, idx_unlabeled, perturbations, ll_constraint=False)
 
-    ## get the attached graoh data 
-    pertrubed_matrix = model.modified_adj
-    ## predict - use model trained on clean data to do predictions based on pertrubed data (pertrubations are generated using ProjMettaAttack) 
-    output = model.Model_forward(features.to('cuda'), pertrubed_matrix.to('cuda'))
+    fake_labels = target_gcn.predict(features.to(device), adj.to(device))
+    fake_labels = torch.argmax(fake_labels, 1).cpu()
+    # Besides, we need to add the idx into the whole process
+    idx_fake = np.concatenate([idx_train,idx_test])
 
-    loss_test = F.nll_loss(output[idx_test], labels[idx_test])
-    acc_test = accuracy(output[idx_test], labels[idx_test])
-    print("Test set results:",
-          "loss= {:.4f}".format(loss_test.item()),
-          "accuracy= {:.4f}".format(acc_test.item()))
+    idx_others = list(set(np.arange(len(labels))) - set(idx_train))
+    fake_labels = torch.cat([labels[idx_train], fake_labels[idx_others]])
 
+    ##### ATTTACK 
+    model.attack(features, adj, fake_labels, idx_fake, perturbations, epochs=200)
+    
+    ##################### Evaluate ######################
+    print('=== testing GCN on Poisoning attack ===')
+    modified_adj = model.modified_adj
+    print(f" Accuracy_ Natural on clean data = {test(adj, target_gcn)}")
 
+    acc_0,model =test(modified_adj, target_gcn)
+    print(f" Misclassification Rate_posinoed data on natural = {1-acc_0}")
+    print(f" Accuracy _ Natural on poisoned data on natural = {acc_0}")
 
+    acc, gcn_robust = test(modified_adj)
+    print(f" Accuracy_ posinoed data on robust = {acc}")
+    print(f" Accuracy_ clean on robust = {test(adj, gcn_robust)}")
 
  
  
-
-
-
-
-
-
    
 if __name__ == "__main__":
     parser = Parser()
